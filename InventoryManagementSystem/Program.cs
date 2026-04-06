826663b722b74d17166123ca5d9890c09f929801
Safa Mohamed <safaa.m.ibrahem@gmail.com>
2026-04-06 16:37:09 +0200

    Add project files.

---
+++ InventoryManagementSystem/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace InventoryManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my inventory management system!..");
+        }
+    }
+}
